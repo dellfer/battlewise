40d638727fccc19c960a31dd2be0a380ed831679
Ken Wante <ken@dellfer.com>
2022-01-19 16:26:37 -0500

    #1 - populate initial blog code snippets

---
+++ src/common/btypes.h
@@ -0,0 +1,38 @@
+/*
+ * btypes.h
+ *
+ * Copyright (C) 2022 DELLFER, INC
+ *
+ * BATTLEWISE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License.
+ *
+ * The code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this code. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This software is available for commercial license, please contact
+ * license@dellfer.com.
+ */
+
+#if (!(defined(BTYPES_H)))
+#define BTYPES_H
+
+
+/*------------------------------------------------------------------------*/
+
+typedef signed char         sbyte;
+typedef signed short        sbyte2;
+typedef signed int          sbyte4;
+
+typedef unsigned char       ubyte;
+typedef unsigned short      ubyte2;
+typedef unsigned int        ubyte4;
+
+typedef ubyte4              bool32;
+
+#endif /* BTYPES_H */
