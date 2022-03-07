5eee9e1fd497f779b5141219b8b23e6f84d3fa5e
Ken Wante <ken@dellfer.com>
2022-03-07 16:25:08 -0500

    Blog-3 updates, part 2

---
+++ src/common/bmacros.h
@@ -0,0 +1,30 @@
+/*
+ * bmacros.h
+ *
+ * Copyright (C) 2022 DELLFER, INC
+ *
+ * BATTLEWISE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License.
+ *
+ * The code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this code. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This software is available for commercial license, please contact
+ * license@dellfer.com.
+ */
+
+#if (!(defined(BMACROS_H)))
+#define BMACROS_H
+
+
+/*------------------------------------------------------------------------*/
+
+#define BMAX(VAL1,VAL2)     ((VAL1 > VAL2) ? (VAL1) : (VAL2))
+
+#endif /* BMACROS_H */
